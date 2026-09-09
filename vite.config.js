155e05aeb93a4c05155530a1cd08fe2e2917a8de
Sididaoudi <sididaoudi24@gmail.com>
2026-09-09 16:47:23 +0200

    ajout du lien dans vite.config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ command }) => {
   return {
     // Utilise le sous-dossier uniquement lors de la compilation (npm run deploy)
     // Reste à la racine "/" en local (npm run dev)
-    base: command === "build" ? "//" : "/",
+    base: command === "build" ? "/Portfolio-react-js/" : "/",
     plugins: [react(), tailwindcss()],
   };
 });
